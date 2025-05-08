1ada50cae620e0c4da9b5d1ade964fc4fdcf87e7
Techmier <72434947+Zahid40@users.noreply.github.com>
2025-05-09 01:17:12 +0530

    .

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, Platform, KeyboardAvoidingView } from "react-native";
 import React, { useState, useRef, useEffect, useCallback } from "react";
 import { Link, Stack } from "expo-router";
 import MenuButton from "😎/components/MenuButton";
@@ -9,34 +9,34 @@ import {
   SelectActiveModelButton,
   SelectActiveModelSheetRef,
 } from "😎/components/selectActiveModel";
-// import { GiftedChat, IMessage } from "react-native-gifted-chat";
+import { GiftedChat, IMessage } from "react-native-gifted-chat";
 
 const index = () => {
   const sheetRef = useRef<SelectActiveModelSheetRef>(null);
   const [activeModel, setActiveModel] = useState<any | null>(null);
 
-  // const [messages, setMessages] = useState<IMessage[]>([]);
+  const [messages, setMessages] = useState([]);
 
-  // useEffect(() => {
-  //   setMessages([
-  //     {
-  //       _id: 1,
-  //       text: "Hello developer",
-  //       createdAt: new Date(),
-  //       user: {
-  //         _id: 2,
-  //         name: "React Native",
-  //         avatar: "https://placeimg.com/140/140/any",
-  //       },
-  //     },
-  //   ]);
-  // }, []);
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+    ]);
+  }, []);
 
-  // const onSend = useCallback((messages = []) => {
-  //   setMessages((previousMessages:any) =>
-  //     GiftedChat.append(previousMessages, messages)
-  //   );
-  // }, []);
+  const onSend = useCallback((messages = []) => {
+    setMessages((previousMessages: any) =>
+      GiftedChat.append(previousMessages, messages)
+    );
+  }, []);
 
   return (
     <>
@@ -69,13 +69,20 @@ const index = () => {
 
       <View className="flex-1">
         <Text>index</Text>
-        {/* <GiftedChat
+        <GiftedChat
+          isKeyboardInternallyHandled={false} // Disable built-in keyboard handling
           messages={messages}
           onSend={(messages) => onSend(messages)}
           user={{
             _id: 1,
           }}
-        /> */}
+        />
+        {Platform.OS === "android" && (
+          <KeyboardAvoidingView
+            behavior="padding"
+            keyboardVerticalOffset={80} // Adjust this value based on your UI
+          />
+        )}
 
         <SelectActiveModel
           ref={sheetRef}
